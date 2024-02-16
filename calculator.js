3efea0ac48b83a9d25257905542bd283caf693e5
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-02-16 09:46:50 +0300

    Add the basic arithmetic functions.

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const add = function(a, b){
+    return a + b;
+} 
+
+const subtract = function(a, b){
+    return a - b;
+}
+
+const multiply = function(a, b){
+    return a * b;
+}
+
+const divide = function (a, b){
+    return a / b;
+}
+
+let firstNumber = ''
+let operator = ''
+let secondNumber = ''
+
+function operate(firstNumber, operator, secondNumber){
+    
+}
