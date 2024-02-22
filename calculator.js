8ceda1bab8de8652fa4c6b2663167aa3c25809f0
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-02-22 08:00:37 +0300

    Add more logic

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
+let buttons = document.querySelectorAll('button');
+let display = document.querySelector('.display');
+let clearBtn = document.getElementById('clear');
+let numbers = document.querySelectorAll('.num');
+let operand = document.querySelectorAll('.operand')
+
+
 const add = function(a, b){
     return a + b;
 } 
@@ -14,10 +21,74 @@ const divide = function (a, b){
     return a / b;
 }
 
+console.log(divide(6, 3))
+
+let currentnumber = display.value;
 let firstNumber = ''
-let operator = ''
 let secondNumber = ''
 
-function operate(firstNumber, operator, secondNumber){
-    
+function operate(firstNumber, secondNumber){
+    if('+' == true ){
+        add(firstNumber, secondNumber);
+    }
+
+    else if ('-' == true ){
+        subtract(firstNumber, secondNumber);
+    }
+
+    else if ('*' == true ){
+        divide(firstNumber, secondNumber);
+    }
+
+    else if ('/' == true ){
+        divide(firstNumber, secondNumber);
+    }
 }
+
+
+numbers.forEach(function(numbers){
+    numbers.addEventListener('click', function(){
+        updateDisplay(numbers.innerHTML);
+    })
+});
+
+function chooseOperation(button){
+    if (button.innerHTML == '+'){
+        operation = '+'
+    }
+
+    else if(button.innerHTML == '-'){
+        operation = '-' 
+    } 
+
+    else if(button.innerHTML == '*'){
+        operation = '*'
+    } 
+
+    else if(button.innerHTML == '÷'){
+        operation = '/'
+    } 
+
+    firstNumber = currentnumber;
+    currentnumber = ''
+}
+operand.forEach(function(button){
+    button.addEventListener('click', function(){
+        chooseOperation(button.innerHTML);
+        updateDisplay(button.innerHTML)
+    })
+})
+
+function updateDisplay(value){
+    if (value === '.' && currentnumber.includes('.')) return
+    display.value += value;
+
+}
+
+
+
+clearBtn.addEventListener('click', function(){
+    display.value = '';
+})
+
+
